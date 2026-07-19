9302a9a177bfc1d68f8bc5822a970ea7a16b3bb2
M A Muhit Joy <muhitjoy2004@gmail.com>
2026-07-19 20:24:40 +0600

    Update Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import facebook from "../../assets/facebook.svg";
 import github from "../../assets/github.svg";
 import linkedIn from "../../assets/linkedin.svg";
 import { DownloadIcon, Mail } from "lucide-react";
-import hero from "../../assets/IMG_20251214.webp";
+import hero from "../../assets/Muhit.JPG";
 import hi from "../../assets/Hi.svg";
 import { Typewriter } from "react-simple-typewriter";
 
